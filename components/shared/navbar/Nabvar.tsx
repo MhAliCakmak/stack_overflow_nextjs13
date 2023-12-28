ca7939de43ad779f0e2bce06666795027cb94fc4
mehmet5643 <202803055@ogr.cbu.edu.tr>
2023-12-28 09:31:18 +0300

    Refactor layout and navbar components

--- components/shared/navbar/Nabvar.tsx
+++ components/shared/navbar/Nabvar.tsx
@@ -1,14 +1,14 @@
-import { SignedIn, UserButton } from "@clerk/nextjs";
-import Image from "next/image";
-import Link from "next/link";
-import React from "react";
-import Theme from "./Theme";
-import MobileNav from "./MobileNav";
-import GlobalSearch from "./GlobalSearch";
+import { SignedIn, UserButton } from '@clerk/nextjs'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import Theme from './Theme'
+import MobileNav from './MobileNav'
+import GlobalSearch from './GlobalSearch'
 
-const Nabvar = () => {
+const Navbar = () => {
   return (
-    <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 dark:shadow-none sm:px-12">
+    <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
       <Link href="/" className="flex items-center gap-1">
         <Image
           src="/assets/images/site-logo.svg"
@@ -16,32 +16,33 @@ const Nabvar = () => {
           height={23}
           alt="DevFlow"
         />
+
         <p className="h2-bold font-spaceGrotesk text-dark-100 dark:text-light-900 max-sm:hidden">Dev <span className="text-primary-500">Overflow</span></p>
       </Link>
-      <GlobalSearch/>
+
+      <GlobalSearch />
+
       <div className="flex-between gap-5">
-        <Theme/>
+        <Theme />
+
         <SignedIn>
-        <UserButton afterSignOutUrl="/"
-        appearance={{
-          elements:{
-            avatarBox:"h-10 w-10" ,
-            name: true,
-            signOutButton: true,
-            signInButton: true,
-            signUpButton: true
-          },variables:{
-            colorPrimary:"#ff7000"
-          }
-         
-        }}
-        />
+          <UserButton 
+            afterSignOutUrl="/"
+            appearance={{
+              elements: {
+                avatarBox: 'h-10 w-10'
+              },
+              variables: {
+                colorPrimary: '#ff7000'
+              }
+            }}
+          />
         </SignedIn>
 
-        <MobileNav/>
+        <MobileNav />
       </div>
     </nav>
-  );
-};
+  )
+}
 
-export default Nabvar;
+export default Navbar
