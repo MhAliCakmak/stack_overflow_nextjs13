5b8b816d3ba7342c53b7a5e0b3a63b858b278d18
mehmet5643 <202803055@ogr.cbu.edu.tr>
2023-12-30 12:56:14 +0300

    Refactor components and add new functionality

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,13 +1,13 @@
-import HomeFilters from "@/components/home/HomeFilters";
-import Filters from "@/components/shared/Filters";
-import NoResult from "@/components/shared/NoResult";
 import QuestionCard from "@/components/cards/QuestionCard";
-import LocalSearch from "@/components/shared/search/LocalSearch";
+import HomeFilters from "@/components/home/HomeFilters";
+import Filter from "@/components/shared/Filters";
+import NoResult from "@/components/shared/NoResult";
+import LocalSearchbar from "@/components/shared/search/LocalSearch";
 import { Button } from "@/components/ui/button";
 import { HomePageFilters } from "@/constants/filter";
 import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
   const questions = [
     {
       _id: "1",
@@ -21,6 +21,7 @@ export default function Home() {
         name: "Mehmet Ali Çakmak",
         picture: "https://avatars.githubusercontent.com/u/60389574?v=4",
       },
+      answers: ["Use React hooks", "Use React hooks"],
       upvotes: 12,
       views: 891,
       createdAt: new Date(),
@@ -40,37 +41,44 @@ export default function Home() {
         picture: "https://avatars.githubusercontent.com/u/60389574?v=7", // Eğer picture bilgisi yoksa burayı doldurabilirsiniz
       },
       upvotes: 22,
+      answers: ["Use React hooks!", "Use React hooks"],
       views: 190,
       createdAt: new Date(), // Veya uygun bir tarih formatında string olarak atanabilir
     },
   ];
+
   return (
     <>
       <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
-        <h1 className="text-dark100_light900 h1-bold ">All Questions</h1>
-        <Link href="/ask-question" className="flex justify-end max-sm:w-full ">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+
+        <Link href="/ask-question" className="flex justify-end max-sm:w-full">
           <Button className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900">
-            Ask a question
+            Ask a Question
           </Button>
         </Link>
       </div>
+
       <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
-        <LocalSearch
+        <LocalSearchbar
           route="/"
-          placeholder="Search for questions..."
-          iconPosition="right"
+          iconPosition="left"
           imgSrc="/assets/icons/search.svg"
+          placeholder="Search for questions"
           otherClasses="flex-1"
         />
-        <Filters
+
+        <Filter
           filters={HomePageFilters}
           otherClasses="min-h-[56px] sm:min-w-[170px]"
           containerClasses="hidden max-md:flex"
         />
       </div>
+
       <HomeFilters />
-      <div className=" mt-10 flex w-full flex-col gap-6 rounded-md px-[45px] py-[36px]">
-        {questions.length > 0 ? (
+
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {questions ? (
           questions.map((question) => (
             <QuestionCard
               key={question._id}
@@ -79,16 +87,15 @@ export default function Home() {
               tags={question.tags}
               author={question.author}
               upvotes={question.upvotes}
+              answers={question.answers && question.answers.length}
               views={question.views}
               createdAt={question.createdAt}
             />
           ))
         ) : (
           <NoResult
-            title="There's no question to show"
-            description="Be the first to break the silence! 🚀 Ask a Question and kickstart the
-            discussion. our query could be the next big thing others learn from. Get
-            involved! 💡"
+            title="There’s no question to show"
+            description="Be the first to break the silence! 🚀 Ask a Question and kickstart the discussion. our query could be the next big thing others learn from. Get involved! 💡"
             link="/ask-question"
             linkTitle="Ask a Question"
           />
--- components/cards/QuestionCard.tsx
+++ components/cards/QuestionCard.tsx
@@ -1,12 +1,12 @@
-import React from "react";
 import Link from "next/link";
-import RenderTags from "../shared/RenderTags";
+import React from "react";
+import RenderTag from "../shared/RenderTags";
 import Metric from "../shared/Metric";
 
 interface QuestionProps {
   _id: string;
   title: string;
-  tags: {
+  tags:{
     _id: string;
     name: string;
   }[];
@@ -16,10 +16,11 @@ interface QuestionProps {
     picture: string;
   };
   upvotes: number;
-  views?: number;
-  answers?: Array<object>;
+  views: number;
+  answers: number;
   createdAt: Date;
 }
+
 const QuestionCard = ({
   _id,
   title,
@@ -31,34 +32,59 @@ const QuestionCard = ({
   createdAt,
 }: QuestionProps) => {
   return (
-    <div className="card-wrapper rounded-[11px] p-9 sm:px-11">
-      <div className="flex flex-col-reverse items-start justify-between gap-5 sm:flex-row ">
+    <div className="card-wrapper rounded-[10px] p-9 sm:px-11">
+      <div className="flex flex-col-reverse items-start justify-between gap-5 sm:flex-row">
         <div>
           <span className="subtle-regular text-dark400_light700 line-clamp-1 flex sm:hidden">
             {String(createdAt)}
           </span>
-          <Link href={`/quesiton/${_id}`}>
+          <Link href={`/question/${_id}`}>
             <h3 className="sm:h3-semibold base-semibold text-dark200_light900 line-clamp-1 flex-1">
               {title}
             </h3>
           </Link>
         </div>
 
+        {/* If signed in add edit delete actions */}
       </div>
+
       <div className="mt-3.5 flex flex-wrap gap-2">
-        {
-          tags.map((tag)=>(
-            <RenderTags  key={tag._id} _id={tag._id} name={tag.name}/>
-          ))
-        }
+        {tags?.map((tags) => (
+          <RenderTag key={tags._id} _id={tags._id} name={tags.name} />
+        ))}
       </div>
+
       <div className="flex-between mt-6 w-full flex-wrap gap-3">
-        <Metric  
+        <Metric
+          imgUrl="/assets/icons/avatar.svg"
+          alt="user"
+          value={author.name}
+          title={` - asked ${String(createdAt)}`}
+          href={`/profile/${author._id}`}
+          isAuthor
+          textStyles="body-medium text-dark400_light700"
+        />
+
+        <Metric
           imgUrl="/assets/icons/like.svg"
           alt="Upvotes"
           value={upvotes}
           title=" Votes"
-          textStyles="small-medium text-dark400_light9"
+          textStyles="small-medium text-dark400_light800"
+        />
+        <Metric
+          imgUrl="/assets/icons/message.svg"
+          alt="message"
+          value={answers}
+          title=" Answers"
+          textStyles="small-medium text-dark400_light800"
+        />
+        <Metric
+          imgUrl="/assets/icons/eye.svg"
+          alt="eye"
+          value={views}
+          title=" Views"
+          textStyles="small-medium text-dark400_light800"
         />
       </div>
     </div>
