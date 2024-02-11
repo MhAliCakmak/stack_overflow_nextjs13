8a8f2fa901f3cb9e8870eb93847ff3478861f25a
MhAliCakmak <202803055@ogr.cbu.edu.tr>
2024-02-11 17:24:54 +0300

    Add community page and tag page components

--- app/(root)/tags/page.tsx
+++ app/(root)/tags/page.tsx
@@ -1,46 +1,45 @@
 import React from "react";
 import LocalSearchbar from "@/components/shared/search/LocalSearch";
 import Filter from "@/components/shared/Filters";
-import { UserFilters } from "@/constants/filter";
+import { TagFilters, UserFilters } from "@/constants/filter";
 import { getAllUsers } from "@/lib/actions/user.action";
 import Link from "next/link";
-import UserCard from "@/components/cards/UserCard";
+import TagCard from "@/components/cards/TagCard";
+import { GetAllTags } from "@/lib/actions/tag.action";
+import NoResult from "@/components/shared/NoResult";
 
 const Page = async () => {
-    const result = await getAllUsers({});
+    const result = await GetAllTags({});
   return (
     <>
       <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
-        <h1 className="h1-bold text-dark100_light900">All Users</h1>
+        <h1 className="h1-bold text-dark100_light900">All Tags</h1>
       </div>
       <div className="mt-11 flex justify-between  gap-5 max-sm:flex-col sm:items-center">
         <LocalSearchbar
           route="/community"
           iconPosition="left"
           imgSrc="/assets/icons/search.svg"
-          placeholder="Search for amazing minds"
+          placeholder="Search for amazing tags"
           otherClasses="flex-1"
         />
         <Filter
-          filters={UserFilters}
+          filters={TagFilters}
           otherClasses="min-h-[56px] sm:min-w-[170px]"
-          containerClasses=""
+          containerClasses=" "
         />
       </div>
       <section className="mt-12 flex flex-wrap gap-4">
     {
-        result.users.length>0 ?(
-            result.users.map((user)=>{
-                return <UserCard key={user._id} user={user}/> 
+        result.tags.length ? (
+            result.tags.map((tag)=>{
+                return <TagCard key={tag._id} tag={tag}/> 
             })
-        ):(
-            <div className="paragraph-regular text-dark200_light800 mx-auto max-w-4xl text-center">
-                <p>No users yet!</p>
-                <Link href="/sign-up" className="mt-2 font-bold text-accent-blue ">
-                Join to be first! 
-                </Link>
-            </div>
-        )
+        ):<NoResult
+        title="No Tags Found"
+        description="No tags found, please try again with different filters"
+        link="/ask-question"
+        />
     }
       </section>
     </>
--- lib/actions/tag.action.ts
+++ lib/actions/tag.action.ts
@@ -1,19 +1,38 @@
-"use server"
+"use server";
 
 import User from "@/database/user.model";
 import { connectToDatabase } from "../mongoose";
-import { GetTopInteractedTagsParams } from "./shared.types";
+import { GetAllTagsParams, GetTopInteractedTagsParams } from "./shared.types";
+import Tag from "@/database/tag.model";
 
-export async function getTopInteracttedTags(params: GetTopInteractedTagsParams) {
+export async function getTopInteracttedTags(
+  params: GetTopInteractedTagsParams
+) {
   try {
     connectToDatabase();
-    const {userId}=params;
+    const { userId } = params;
     const user = await User.findById(userId);
 
-    if(!user){
-      throw new Error("User not found")
+    if (!user) {
+      throw new Error("User not found");
     }
-    return [{_id:"1",name:"tag1"},{_id:"2",name:"tag2"}]; //@todo implement this method, for now just returning dummy data
+    return [
+      { _id: "1", name: "tag1" },
+      { _id: "2", name: "tag2" },
+    ]; //@todo implement this method, for now just returning dummy data
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function GetAllTags(params: GetAllTagsParams) {
+  try {
+    connectToDatabase();
+
+    const tags = await Tag.find({});
+
+    return { tags };
   } catch (error) {
     console.log(error);
     throw error;
