1133a9ea117c60cd3af35d2cc3c8ffb9f3dd5c3b
MhAliCakmak <202803055@ogr.cbu.edu.tr>
2024-02-12 18:22:10 +0300

    Add Answer component and PrismJS styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { Inter, Space_Grotesk } from 'next/font/google'
 import type { Metadata } from 'next';
 
 import './globals.css';
+import "../styles/prisimjs.css"
 import { ThemeProvider } from '@/context/ThemeProvider';
 
 const inter = Inter({
